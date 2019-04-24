3ca6b3c6043169ab7b4a8cc1cfbd209eb957b258
Fahmisutansyah <fahmisutansyah@gmail.com>
2019-04-24 17:52:13 +0700

    add modals to login and register, not done

--- app.js
+++ app.js
@@ -2,26 +2,21 @@ const port  = process.env.PORT || 7777
 const session = require('express-session')
 const express = require('express')
 const app = express()
-// const routes = require('./routes')
-// const gameRoute = require('./routes/game')
-// const itemRoute = require('./routes/item')
-// const propertyRoute = require('./routes/property')
+const checkLogin = require('./middlewares/checkLogin')
 
 const bodyParser = require('body-parser');
-// app.locals.scoreLetter = require('./helpers').scoreLetter
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({secret: 'tests'}))
 
 app.set('view engine','ejs')
-// app.use('/', routes)
-
+app.use(express.static('public'))
 app.use('/login', require('./routes/login'))
-
-
-// app.use('/item', itemRoute)
-// app.use('/property', propertyRoute)
-// app.use('/game', gameRoute)
+app.use('/register', require('./routes/register'))
+app.use('/logout',require('./routes/logout'))
+app.use(checkLogin)
+app.use('/home', require('./routes/home'))
 
 app.listen(port, ()=>{
     console.log(`server is running at port ${port}`)
---
+++ routes/logout.js
@@ -0,0 +1,14 @@
+const route = require('express').Router()
+const {Player} = require('../models')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
+route.use(bodyParser.json())
+route.use(bodyParser.urlencoded({extended:true}))
+
+route.get('/',(req,res)=>{
+    delete req.session.player
+    // res.redirect('/login')
+    res.send(req.session)
+})
+
+module.exports = route
