c2d30716a0bcc37ce52778f3fc29a6381de944fe
Fahmisutansyah <fahmisutansyah@gmail.com>
2019-04-26 09:49:16 +0700

    adding low level items

--- middlewares/permission.js
+++ middlewares/permission.js
@@ -13,8 +13,6 @@ module.exports = (req,res,next) =>{
             throw new Error(`You have to sell an item that has the same type of item that you want to buy!`)
         }else if (player.level < item.minLevel){
             throw new Error(`You don't meet the level requirement!`)
-        }else if (owned&&owned.type === item.type){
-            throw new Error(`You already have the same type of item! Sell it first!`)
         }else if (player.money < item.price){
             throw new Error(`You don't have enough money to buy that item!`)
         }else{
---
+++ seeders/20190426024255-addWeapons.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+   return queryInterface.bulkInsert('Items', [{
+    name: 'Ripped Cardboard',
+    type: "sheild",
+    power: 8,
+    trait: 60,
+    price: 200,
+    minLevel: 1,
+    img: "/images/cardboard1.png"
+  },{
+    name: 'Kithcen Knife',
+    type: "weapon",
+    power: 6,
+    trait: 60,
+    price: 200,
+    minLevel: 1,
+    img: "/images/kitchenKnife.png"
+  },{
+    name: 'Glock 17',
+    type: "weapon",
+    power: 12,
+    trait: 50,
+    price: 450,
+    minLevel: 3,
+    img: "/images/glock17.png"
+  }
+], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
